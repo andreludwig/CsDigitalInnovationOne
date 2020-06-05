6f9d9c410fef78771903ea6de04ce1e3f4915b69
andreludwig <andre@roadseg.com.br>
2020-06-05 20:29:05 -0300

    Inserindo cadastro de alunos

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
@@ -13,7 +15,21 @@ namespace DigitalInnovationOne
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //ToDo: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.Nota = nota;
+                        }else{
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
                         //ToDo: listar alunos
@@ -31,6 +47,7 @@ namespace DigitalInnovationOne
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
